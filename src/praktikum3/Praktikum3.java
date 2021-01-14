85f6eafa716b2260ef76d3427f593f73fd6450a5
adindhaprahesthy29rpl <adindha_prahesthy_29rpl@student.smktelkom-mlg.sch.id>
2021-01-14 15:07:15 +0700

    Praktikum 3

---
+++ src/praktikum3/Praktikum3.java
@@ -0,0 +1,49 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package praktikum3;
+
+/**
+ *
+ * @author MOKLET-2
+ */
+public class Praktikum3 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        // Deklarasi Variabel dan Menyimpan Nilai
+        int p,u,j=5,i=5,f =6;
+        
+        // Menampilkan Judul dari Program
+        System.out.println("============================");
+        System.out.println("PROGRAM MENAMPILKAN BENTUK Z");
+        System.out.println("============================");
+        
+        // Perulangan 
+        // p = 1, p Kurang Dari atau Sama Dengan j, Maka p++ (Perulangan dimulai dari 1 dan akan terus berulang sampai kurang dari / sama dengan p)
+              for(p=1;p<=j;p++){
+                  
+                // u = 5, u Lebih Dari atau Sama Dengan 1, Maka u-- (Perulangan dimulai dari 1 dan akan terus berkurang sampai sama dengan 5)
+                for(u=5;u >=1;u--){
+                
+                    // Percabangan
+                    if (p==u)
+                        System.out.print("@");
+                    else if  
+                        (p==1 || p==5)
+                        System.out.print("@");
+                    else 
+                            System.out.print(" ");
+                    }
+                 System.out.println();
+                }    
+            }
+        }
+    
+
+
